5513b71b969a4852c4ce3f73b026c34474fe8ebd
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-01-28 18:37:02 -0800

    Final touches for full functionality

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.use(express.static('public'));
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
-})
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
